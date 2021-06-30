a0c40afc8cdeb6f3b57e05b5e5190bfd68c395ec
Dalton Rogerio Alvarez <dalton@gestaointeligentesoftware.com.br>
2021-06-30 18:31:50 -0300

    First commit

--- app/Models/Api/Client.php
+++ app/Models/Api/Client.php
@@ -8,4 +8,48 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    protected $table = 'clients';
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = ['receive_news' => 'boolean',
+                        'active' => 'boolean'
+    ];
+    /**
+     * @var array
+     */
+    protected $filable = [
+        'name',
+        'email',
+        'phone',
+        'zipcode',
+        'address',
+        'address_number',
+        'receive_news',
+    ];
+    /**
+     * @var array
+     */
+    protected $hidden = [
+        'neighborood_id',
+        'city_id',
+        'fname',
+        'password',
+        'active',
+        'created_at',
+        'updated_at',
+    ];
+    /**
+     * define phonetics attribute
+     */
+    public function setFnameAttribute($value)
+    {
+        $this->attributes['fname'] = phonetics($value);
+    }
+
+
 }
